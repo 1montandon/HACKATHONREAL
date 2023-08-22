2d1f0cb7d1747405214929bb3c2bff4aa6f38ed9
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-08-22 11:16:52 -0300

    fix bugs

--- Frontend/src/api/categorias.js
+++ Frontend/src/api/categorias.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-<<<<<<< HEAD
+
 export default class CategoriasApi {
   async buscarTodasAsCategorias() {
     const response = await axios.get("http://localhost:3000/categorias/");
@@ -26,32 +26,3 @@ export default class CategoriasApi {
     return response.data;
   }
 }
-=======
-export default class UsuariosApi{
-    async buscarTodasAsCategorias() {
-        const response = await axios.get("http://localhost:3000/categorias/");
-        return response.data;
-      }
-    
-      async buscarCategoria(id) {
-        const response = await axios.get(`http://localhost:3000/categorias/${id}`);
-        return response.data;
-      }
-    
-      async adicionarCategoria(categoria) {
-        const response = await axios.post("http://localhost:3000/categorias/", categoria);
-        return response.data;
-      }
-    
-      async excluirCategoria(id) {
-        const response = await axios.delete(`http://localhost:3000/categorias/${id}`);
-        return response.data;
-      }
-    
-      async atualizarCategoria(categoria) {
-        const response = await axios.put(`http://localhost:3000/categorias/${categoria.id}`,categoria);
-        return response.data;
-      }
-    
-}
->>>>>>> 9375c1a86214c9c22e464a8ec3ea24626f578dcd
--- Frontend/src/api/usuarios.js
+++ Frontend/src/api/usuarios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 export default class UsuariosApi{
-<<<<<<< HEAD
   async buscarTodosUsuarios() {
     const response = await axios.get("http://localhost:3000/categorias/");
     return response.data;
@@ -24,15 +23,4 @@ export default class UsuariosApi{
     const response = await axios.put(`http://localhost:3000/categorias/${categoria.id}`,categoria);
     return response.data;
   }
-=======
-    async adicionarUsuarios(usuarios){
-        const response = await axios.post("http://localhost:3000/usuarios/", usuarios);   
-        return response.data
-    }
-    async removerUsuarios(id){
-        const response = await axios.delete(`http://localhost:3000/usuarios/${id}`);
-        return response.data
-    }
-    async 
->>>>>>> 9375c1a86214c9c22e464a8ec3ea24626f578dcd
 }

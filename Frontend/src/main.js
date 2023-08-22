743677ea71de1ca26345539e5cb22ccd8f77eb7b
TinaPera <valentina.pera2007@gmail.com>
2023-08-22 11:20:01 -0300

    FoodCard Logic Finished

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -7,11 +7,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 
-library.add(faEnvelope, faEye, faEyeSlash, faStar)
+library.add(faEnvelope, faEye, faEyeSlash, faStar, faDollarSign)
 
 const app = createApp(App)
 

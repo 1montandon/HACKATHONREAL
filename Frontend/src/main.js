e500ff8c6596fcc9f201911849e79ee5250d9f98
= <=>
2023-08-22 10:58:47 -0300

    inicio

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faEnvelope, faEye, faEyeSlash)
 
@@ -16,4 +16,4 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
-app.component("font-awesome-icon", FontAwesomeIcon )
+app.component("font-awesome-icon", FontAwesomeIcon, faUser )
